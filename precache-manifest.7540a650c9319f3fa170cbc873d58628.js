957e49d6c28837564ef66d78a50f39b3565381a9
thisguycodez <guyton578@gmail.com>
2020-01-25 02:59:03 -0500

    Updates

---
+++ precache-manifest.7540a650c9319f3fa170cbc873d58628.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ab1fafbbeb176016cfbc0ad58a1ebcb",
+    "url": "/exam1/index.html"
+  },
+  {
+    "revision": "d5dd709d2b743de38b65",
+    "url": "/exam1/static/css/main.270beb3d.chunk.css"
+  },
+  {
+    "revision": "909fe29b15776cebca8e",
+    "url": "/exam1/static/js/2.e5220282.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/exam1/static/js/2.e5220282.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d5dd709d2b743de38b65",
+    "url": "/exam1/static/js/main.c583ac34.chunk.js"
+  },
+  {
+    "revision": "b6b355791bb16cf4af61",
+    "url": "/exam1/static/js/runtime-main.f31eb0ff.js"
+  }
+]);
